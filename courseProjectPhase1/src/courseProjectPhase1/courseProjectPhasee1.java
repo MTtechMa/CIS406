59fbfe41184b2956e8f61986b7573028e2ce8165
ucctn <chetan.singhal@ucertify.com>
2023-10-02 23:54:59 -0700

    commit

---
+++ courseProjectPhase1/src/courseProjectPhase1/courseProjectPhasee1.java
@@ -0,0 +1,12 @@
+package courseProjectPhase1;
+
+public class courseProjectPhasee1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Congratulations on your first Java program");
+	}
+
+}
